0a000a6732e769ef1a08337668b1d223c5cc0065
Ethan Tidmore <ethantidmore06@gmail.com>
2024-04-07 21:18:38 -0500

    Add setuparch.sh script

---
+++ setuparch.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+
+# My Arch setup script (base-devel is needed for this)
+
+# Create used directories
+
+mkdir -p /home/$USER/.config/bspwm /home/$USER/.config/sxhkd /home/$USER/.local/bin \
+	/home/$USER/.local/src /home/$USER/.local/wallpapers
+
+# Download and install yay (its an aur manager)
+
+git clone https://aur.archlinux.org/yay.git
+
+cd yay
+
+makepkg -si
+
+cd .. # Go back to Dotfiles
+
+# Install usefull software
+
+yay -S zsh zsh-syntax-highlighting zsh-autosuggestions xorg bspwm sxhkd git nano feh picom \
+       	unclutter dmenu lxappearance ttf-hack-nerd xcursor-breeze google-chrome
+
+# Install dotfiles
+
+cp .xinitrc /home/$USER
+
+cp .fehbg /home/$USER
+
+cp .vimrc /home/$USER
+
+cp .zshrc /home/$USER
+
+cp .bswpmrc /home/$USER/.config/bspwm
+
+cp .sxhkdrc /home/$USER/.config/sxhkd
+
+cp landscape.jpg /home/$USER/.local/wallpapers
+
+# setup wallpaper
+
+feh --bg-scale /home/$USER/.local/wallpapers/landspace.jpg
+
+# Install vim plug
+
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+# Lastly setup my st build
+
+cd st-0.8.4
+
+make
+
+sudo make install
+
+cd /home/$USER
