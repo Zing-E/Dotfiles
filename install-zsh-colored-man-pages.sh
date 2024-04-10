41da230491277ebef4d35a14139f335a26c80081
Ethan Tidmore <ethantidmore06@gmail.com>
2024-04-10 10:56:43 -0500

    Update install script for colored man pages and clean whitespace

--- install-zsh-colored-man-pages.sh
+++ install-zsh-colored-man-pages.sh
@@ -4,8 +4,10 @@
 
 PKG_NAME="zsh-colored-man-pages"
 PKG_DIR="/usr/share/zsh/plugins/$PKG_NAME"
+# For MacOS
+#PKG_DIR="/opt/homebrew/share/$PKG_NAME"
 
-wget https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/plugins/colored-man-pages/colored-man-pages.plugin.zsh -O $PKG_NAME.zsh
+curl -o $PKG_NAME.zsh https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/plugins/colored-man-pages/colored-man-pages.plugin.zsh 
 
 chmod 0775 $PKG_NAME.zsh
 
